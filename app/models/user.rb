728fbbf5a5d81c3179227b7ddfbe66306341dc44
Johnny Santiago <JohnnyIn2A@Gmail.com>
2013-03-15 02:52:06 -0400

    created relationship between user and status

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   validates :name, :presence => true
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
 
-  # TESTING
+  has_many :statuses
 end
