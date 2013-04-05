58abb0ac46c4106a3d5d3ea99fdd3ab33d423ea9
Johnny Santiago <JohnnyIn2A@Gmail.com>
2013-04-05 12:03:59 -0400

    Added tests for model attribute presence validation

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Status do
-	before { @status = Status.new(name: "Example User", email: "user@example.com") }
+	before { @status = Status.new(status: "Example Status", image_url: "http://www.images.com/exampleimage.jpg", geocode: "41.9082662 -88.060051") }
 
-	subject { @user }
+	subject { @status }
 
-	it { should respond_to(:name) }
-	it { should respond_to(:email) }
+	it { should respond_to(:status) }
+	it { should respond_to(:image_url) }
+	it { should respond_to(:geocode) }
+
+	it { should be_valid }
+
+	describe "when status is not present" do
+		before { @status.status = " " }
+		it { should_not be_valid }
+	end
 end
